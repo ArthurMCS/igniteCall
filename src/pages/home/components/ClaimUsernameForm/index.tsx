f92b8bedd0e12544f73af13276a92be49af6a3d2
Arthur Moreira <arthurmcs96@gmail.com>
2023-01-24 21:51:43 -0300

    config react-hook-form and zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,30 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUserNameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUserNameFormData = z.infer<typeof claimUserNameFormSchema>
+
 export default function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUserNameFormData>()
+
+  async function handleClaimUserName(data: ClaimUserNameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUserName)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
