7c8aaa4ab8d57460eb114ddc2265c0b4d08b0717
Arthur Moreira <arthurmcs96@gmail.com>
2023-01-26 17:50:14 -0300

    create an alert when trying to create an existing user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,8 @@ import { Container, Form, FormError, Header } from './styles'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
+/* import { AxiosError } from 'axios' */
 
 const registerFormSchema = z.object({
   username: z
@@ -47,8 +49,12 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        return alert(err.response.data.message)
+      }
+
+      console.error(err)
     }
   }
 
